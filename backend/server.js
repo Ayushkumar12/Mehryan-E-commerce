3c21e6a0cc36b7188b042e5a4420876037813de0
Ayushkumar12 <ayushkumarak112003@gmail.com>
2025-11-08 11:48:05 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,7 @@ app.locals.db = db;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://mehryan-e-commerce.vercel.app/',
   credentials: true
 }));
 
